05e933aa786faf0525a16a616322f96b941741a3
kasperbb <kasperbb98@gmail.com>
2022-02-14 20:38:35 +0100

    Adds shuffle array util

---
+++ utils/shuffleArray.ts
@@ -0,0 +1,27 @@
+export function shuffleArray(array: any[]) {
+	let currentIndex = array.length,
+		randomIndex
+
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex--
+		;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+	}
+
+	return array
+}
+
+export function immutableShuffleArray(array: any[]) {
+	const arr = [...array]
+
+	let currentIndex = arr.length,
+		randomIndex
+
+	while (currentIndex != 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex--
+		;[arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]
+	}
+
+	return arr
+}
